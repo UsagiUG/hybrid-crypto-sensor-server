79f85710ba4cdd118b5dbe7a400fd7f88d7e978b
Galih Prabasidi <galihprabasidi@gmail.com>
2026-05-18 19:28:01 +0700

    server generates rsa key at start

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 import express from 'express'
 import router from './src/route.js'
 import config from './src/config.js'
+import {getLatestKey, insertKey} from './data/queries.js'
+import RSAUtils from './src/rsaUtils.js'
+import { nanoid } from 'nanoid'
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
-app.listen(3000, () => {
+app.listen(3000, async () => {
   console.log('Server is running on port 3000')
+
+  try{
+    const existing = getLatestKey.get()
+    if (!existing) {
+      const keys = await RSAUtils.generateKeyPair()
+      insertKey.get(nanoid(), keys.publicKey, keys.privateKey, Date.now())
+      console.log('RSA keys generated successfully')
+    } else {
+      console.group('RSA keys already exist')
+    }
+  } catch (err) {
+    console.error('Error generating RSA keys', err.message)
+  }
 })
